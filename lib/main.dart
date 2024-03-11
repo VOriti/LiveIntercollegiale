787253fa09b4917e3dac38f04ef8254b66368f31
VOriti <vincenzo.oriti@gmail.com>
2024-03-11 14:41:09 +0100

    webview funzionante con JS unrestricted

--- lib/main.dart
+++ lib/main.dart
@@ -27,22 +27,37 @@ class _WebViewAppState extends State<WebViewApp> {
   void initState() {
     super.initState();
     controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted) //parametro aggiunto altrimenti non funziona nessuna pagina che utilizzi javascript
       ..loadRequest(
-        Uri.parse('http://live.intercollegiale.it/index.php'),
+        Uri.parse('https://live.intercollegiale.it'),
       );
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter WebView'),
+
+//appbar predefinita con titolo e tasti di navigazione
+/*       appBar: AppBar(
+        title: const Text('Live Intercollegiale'),
 
         actions: [
           NavigationControls(controller: controller),
         ],
       ),
-      body: WebViewStack(controller: controller),       // MODIFY
+*/
+
+// appbar nuova che evita la sovrapposizione con la barra di android ma non mostra nulla
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top),
+        child: SizedBox(
+          height: MediaQuery.of(context).padding.top,
+        ),
+      ),
+
+      // corpo dello schermo
+      body: WebViewStack(
+          controller: controller),
     );
   }
 }
